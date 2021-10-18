c31b20b3a059ac4facee13541903e86af07ea91d
codpez <udelgado99@gmail.com>
2021-10-17 20:36:55 -0500

    En esta version se agrego una funcion para mandar el resultado del descuento al html a traves de js.

--- Taller-2/descuentos.js
+++ Taller-2/descuentos.js
@@ -3,3 +3,13 @@ function descuento(precioOriginal,porcentaje){
     precioFinal=precioOriginal-rebaja;
     return precioFinal
 }
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceVal = inputPrice.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountval= inputDiscount.value;
+    const precioConDescuento = descuento(priceVal,discountval);
+    console.log(precioConDescuento);
+    const resu = document.getElementById("Resultado");
+    resu.innerText = "El precio con descuento es $" + precioConDescuento;
+}
