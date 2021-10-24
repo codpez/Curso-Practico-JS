e1b9afd553e517d0c38bc81ce96283e105655903
codpez <udelgado99@gmail.com>
2021-10-23 23:18:36 -0500

    La primera funcionm de promedio

--- Taller-3/promedio.js
+++ Taller-3/promedio.js
@@ -1,5 +1,14 @@
 const lista1 = [100, 200,300,500,];
-let sumaLista1 = 0;
- for(let i=0; i<lista1.length; i++){
-     sumaLista1 = sumaLista1 + lista1[i];
+
+ function calcularPromedio(LS) {
+  //  let sumaLista1 = 0;
+  //  for(let i=0; i<LS.length; i++){
+    //    sumaLista1 = sumaLista1 + LS[i];
+  //  }
+  const sumaList = LS.reduce(
+      function(valorAcumulado=0, nuevoElemento) {
+        return valorAcumulado+nuevoElemento;
+      }
+  );
+  return sumaList;
  }
